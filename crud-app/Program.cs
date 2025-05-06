ebd26d55be3b71e46ef13d00499e6ef52d779a58
AC-tiger-92376 <actiger1215@gmail.com>
2025-05-06 09:58:15 -0700

    Initial commit

---
+++ crud-app/Program.cs
@@ -0,0 +1,51 @@
+using crud_app.Models;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+// Set the connection string from the environment variable
+builder.Configuration["ConnectionStrings:DefaultConnection"] = 
+    Environment.GetEnvironmentVariable("DefaultConnection");
+
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+/*
+builder.Services.AddDbContext<StudentContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentConnection")));
+*/
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
